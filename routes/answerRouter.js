2073a350985e98b0a2dbc8e6a8d566d1dfbf488e
Harish Bathina <harish.bathina@ggktech.local>
2018-10-12 11:58:47 +0530

    jwt

--- routes/answerRouter.js
+++ routes/answerRouter.js
@@ -1,28 +1,39 @@
-var router =require('express').Router();
-var User=require('../models/User');
-var Question = require('../models/Question');
+var router = require("express").Router();
+var User = require("../models/User");
+var Question = require("../models/Question");
+const verifyToken = require("../verifyToken");
+var Tag = require("../models/Tag");
+var Comment = require("../models/Comment");
+var Answer = require("../models/Answer");
 
-var Tag=require('../models/Tag');
-var Comment = require('../models/Comment');
-var Answer = require('../models/Answer');
-
-module.exports=router;
+module.exports = router;
 
 //post answer for a question
-router.post('/',function(req,res,next){
-    Answer.create(req.body)
-    .then(function(answer)
-    {
-        res.send(answer)
-    })
-})
+router.post("/", verifyToken, function(req, res, next) {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.status(403).send("Invalid Token");
+    } else {
+      Answer.create(req.body).then(function(answer) {
+        res.send(answer);
+      });
+    }
+  });
+});
 
 //get all the answers in the database along with their questionId's and userId's
-router.get('/',function(req,res,next){
-    Answer.findAll({
-        include:[{
-            all:true,
-        }]
-    }).then(res.send.bind(res))
-})
-
+router.get("/", verifyToken, function(req, res, next) {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.status(403).send("Invalid Token");
+    } else {
+      Answer.findAll({
+        include: [
+          {
+            all: true
+          }
+        ]
+      }).then(res.send.bind(res));
+    }
+  });
+});
--- routes/questionRouter.js
+++ routes/questionRouter.js
@@ -6,30 +6,43 @@ var Tag = require("../models/Tag");
 var Op = Sequelize.Op;
 var Comment = require("../models/Comment");
 var Answer = require("../models/Answer");
+const verifyToken = require("../verifyToken");
 
 module.exports = router;
 var second = {};
 
 //post the questions to the questions table and after completion post tags to tags table
-router.post("/", function(req, res, next) {
-  const length = req.body.tags.length;
-  var tags = req.body.tags.slice();
-  delete req.body.tags;
-  Question.create(req.body)
-    .then(function(question) {
-      for (i = 0; i < length; i++) {
-        second.tag = tags[i];
-        second.question_id = question.id;
+router.post("/", verifyToken, function(req, res, next) {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.status(403).send("Invalid Token");
+    } else {
+      const length = req.body.tags.length;
+      var tags = req.body.tags.slice();
+      delete req.body.tags;
+      Question.create(req.body)
+        .then(function(question) {
+          for (i = 0; i < length; i++) {
+            second.tag = tags[i];
+            second.question_id = question.id;
 
-        Tag.create(second).then(function(tag) {});
-      }
-    })
-    .then(res.send("success"));
+            Tag.create(second).then(function(tag) {});
+          }
+        })
+        .then(res.send("success"));
+    }
+  });
 });
 
 //get all the questions in the questions table
-router.get("/", function(req, res, next) {
-  Question.findAll({ include: [{ all: true }] }).then(res.send.bind(res));
+router.get("/", verifyToken, function(req, res, next) {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.status(403).send("Invalid Token");
+    } else {
+      Question.findAll({ include: [{ all: true }] }).then(res.send.bind(res));
+    }
+  });
 });
 //     Question.findOne({
 //         where:{
@@ -45,62 +58,74 @@ router.get("/", function(req, res, next) {
 // })
 
 //get the details of question and user associated with question_id
-router.get("/:id", function(req, res, next) {
-  Question.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: [
-      {
-        all: true
-      }
-    ]
-  }).then(function(question) {
-    if (!question) res.send("not found");
-    else res.send(question);
+router.get("/:id", verifyToken, function(req, res, next) {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.status(403).send("Invalid Token");
+    } else {
+      Question.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [
+          {
+            all: true
+          }
+        ]
+      }).then(function(question) {
+        if (!question) res.send("not found");
+        else res.send(question);
+      });
+    }
   });
 });
 
 //get answers and comments associated with a question using questionId
-router.get("/info/:questionId", function(req, res, next) {
-  var arr = [];
-  Answer.findAll({
-    where: {
-      questionId: req.params.questionId
-    },
-    attributes: ["id"]
-  })
-    .then(function(results) {
-      results.map(result => {
-        arr.push(result.dataValues.id);
-      });
-      console.log(arr);
-    })
-    .then(function() {
-      console.log(arr);
-      Comment.findAll({
+router.get("/info/:questionId", verifyToken, function(req, res, next) {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.status(403).send("Invalid Token");
+    } else {
+      var arr = [];
+      Answer.findAll({
         where: {
-          answerId: {
-            [Op.in]: arr
-          }
+          questionId: req.params.questionId
         },
-        include: [{ all: true, include: [{ all: true }] }]
-      }).then(function(info) {
-        Answer.findAll({
-          where: {
-            questionId: req.params.questionId
-          },
-          include: [
-            {
-              all: true
-            }
-          ]
-        }).then(function(info1) {
-          info.push(info1);
-          res.send(info);
+        attributes: ["id"]
+      })
+        .then(function(results) {
+          results.map(result => {
+            arr.push(result.dataValues.id);
+          });
+          console.log(arr);
+        })
+        .then(function() {
+          console.log(arr);
+          Comment.findAll({
+            where: {
+              answerId: {
+                [Op.in]: arr
+              }
+            },
+            include: [{ all: true, include: [{ all: true }] }]
+          }).then(function(info) {
+            Answer.findAll({
+              where: {
+                questionId: req.params.questionId
+              },
+              include: [
+                {
+                  all: true
+                }
+              ]
+            }).then(function(info1) {
+              info.push(info1);
+              res.send(info);
+            });
+          });
         });
-      });
-    });
+    }
+  });
 });
 
 // Question.findById(req.params.id)
